d733edc8b3dfbe26bd29e680e968026c8911ea8f
christian-dias <christian.dias@aurum.com.br>
2022-03-12 11:50:25 -0300

    teste arrumando

--- src/test/java/com/hefesto/ceos/CeosApplicationTests.java
+++ src/test/java/com/hefesto/ceos/CeosApplicationTests.java
@@ -16,7 +16,6 @@ class CeosApplicationTests {
 	void contextLoads() {
         assertEquals(Classe.ADMINISTRADOR.ordinal(), 0);
         assertEquals(Classe.USUARIO.ordinal(), 1);
-		assertEquals(1 , 0);
 	}
 
 }
